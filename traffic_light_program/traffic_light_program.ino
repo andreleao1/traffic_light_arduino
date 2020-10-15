285f6cfdb3df331892bb3648900463888133200e
andreleao1 <andre.gustavo.ls@hotmail.com>
2020-10-15 02:26:51 -0300

    Definindo os pinos de saída para os led

--- traffic_light_program/traffic_light_program.ino
+++ traffic_light_program/traffic_light_program.ino
@@ -9,7 +9,10 @@
 const red_c_l = A0, yellow_c_l = A1, green_c_l = A2, red_p_l = A3, green_p_l = A4, red_c_r = A5, yellow_c_r = 2, green_c_r = 3, red_p_r = 4, green_p_r = 5;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(red_c_l, OUTPUT); pinMode(yellow_c_l, OUTPUT); pinMode(green_c_l, OUTPUT);
+  pinMode(red_p_l, OUTPUT); pinMode(green_p_l, OUTPUT); pinMode(red_c_r, OUTPUT);
+  pinMode(yellow_c_rc_l, OUTPUT); pinMode(green_c_r, OUTPUT); pinMode(red_p_r, OUTPUT);
+  pinMode(green_p_r, OUTPUT);
 
 }
 
