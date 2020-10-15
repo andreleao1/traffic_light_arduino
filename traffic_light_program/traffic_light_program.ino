9c692937156a509be9b2ec58da86bdcc178f0e34
andreleao1 <andre.gustavo.ls@hotmail.com>
2020-10-15 02:22:19 -0300

    Definindo as variáveis dos leds

--- traffic_light_program/traffic_light_program.ino
+++ traffic_light_program/traffic_light_program.ino
@@ -1,3 +1,13 @@
+/*
+ * Definindo alias para os leds, seguindo o seguinte padrão:
+ * <cor_pedestre ou carro_lado>
+ * onde "c" é para carro e "p" para pedestre
+ * e "l" para esquerda e "r" para direita
+ * 
+ * Exemplo: red_c_l (led vermelho, para carros da esquerda).
+ */
+const red_c_l = A0, yellow_c_l = A1, green_c_l = A2, red_p_l = A3, green_p_l = A4, red_c_r = A5, yellow_c_r = 2, green_c_r = 3, red_p_r = 4, green_p_r = 5;
+
 void setup() {
   // put your setup code here, to run once:
 
